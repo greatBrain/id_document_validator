c0e45408380e3cd86a1aa2fb4f750ac99c715a2e
erick Misael <erickFania@gmail.com>
2020-07-31 12:06:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description = """A simple Identity Document Characters Validator (just for DNI and cedula identity documents)
     you use it as follows: from validator.validato import Validator. Then, you call the is_valid method:
     Validato.is_valid(document_name, document_numeration), this must return or True or False""",
-    url = "https://github.com/greatBrain/identity-validator", 
+    url = "https://github.com/greatBrain/id_document_validator", 
     packages = setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
